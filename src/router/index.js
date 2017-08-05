e535457b319f5f088e5190c2f8b1136c242a192d
wangdabao <614692339@qq.com>
2017-08-05 17:16:59 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -72,10 +72,17 @@ export default new Router({
     }
   ],
   scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { x: 0, y: 0 }
+    // 如果你的連結是帶 # 這種
+    // to.hash 就會有值(值就是連結)
+    // 例如 #3
+    console.log(savedPosition)
+    if (to.hash) {
+      return {
+        // 這個是透過 to.hash 的值來找到對應的元素
+        // 照你的 html 來看是不用多加處理這樣就可以了
+        // 例如你按下 #3 的連結，就會變成 querySelector('#3')，自然會找到 id = 3 的元素
+        selector: to.hash
+      }
     }
   }
 })
